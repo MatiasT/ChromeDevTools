12e4a7786ba31aa3e95cb56cdddce731678635ec
Matias Teragni <matias.teragni@gmail.com>
2018-04-25 13:11:56 -0300

    Added Chrome wrapper. Added an auxiliary class to hold all the path and multiop stuff

---
+++ Tera.ChromeDevTools/Tera.ChromeDevTools/Chrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Tera.ChromeDevTools
+{
+    public class Chrome
+    {
+        private Process chromeProcess;
+        private string directoryInfo;
+        private int remoteDebuggingPort;
+
+
+        public Chrome(int remoteDebuggingPort = 9222, bool headless = true)
+        {
+            directoryInfo = ChromeUtils.CreateTempFolder();
+            var remoteDebuggingArg = $"--remote-debugging-port={remoteDebuggingPort}";
+            var userDirectoryArg = $"--user-data-dir=\"{directoryInfo}\"";
+            var chromeProcessArgs = $"{remoteDebuggingArg} {userDirectoryArg} --bwsi --no-first-run";
+
+            if (headless == true)
+            {
+                chromeProcessArgs += " --headless";
+            }
+
+            Process chromeProcess = Process.Start(ChromeUtils.GetChromePath(), chromeProcessArgs);
+
+
+        }
+
+
+    }
+}
---
+++ Tera.ChromeDevTools/Tera.ChromeDevTools/ChromeUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Tera.ChromeDevTools
+{
+    internal static class ChromeUtils
+    {
+        public static string GetChromePath() {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return "google-chrome";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+            }
+            else
+            {
+                throw new InvalidOperationException("Unknown or unsupported platform.");
+            }
+        }
+        public static string CreateTempFolder() {
+            string path = Path.GetRandomFileName();
+            return Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), path)).FullName;
+        }
+
+    }
+}
