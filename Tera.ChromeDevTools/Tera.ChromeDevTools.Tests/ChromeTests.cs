627a74c339a6d1a95b736d979b619ded77450f29
Matias Teragni <matias.teragni@gmail.com>
2018-04-25 13:12:06 -0300

    Added a test for the process creation

---
+++ Tera.ChromeDevTools/Tera.ChromeDevTools.Tests/ChromeTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Tera.ChromeDevTools.Tests
+{
+    [TestClass]
+    public class ChromeTests
+    {
+        [TestMethod]
+        public void ChromeOpenTest()
+        {
+            var prev = Process.GetProcessesByName("chrome");
+            Chrome c = new Chrome();
+            var post = Process.GetProcessesByName("chrome");
+            Assert.IsTrue(post.Length > prev.Length,"No chrome processes were created on the initialization of a Chrome instance");          
+
+        }
+    }
+
+}
